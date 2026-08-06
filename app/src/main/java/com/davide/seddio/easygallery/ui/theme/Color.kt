d7c5910ba0c4380829f2ead852b90e23e6b3d4e1
davide <davide.seddio@gmail.com>
2026-08-06 10:30:59 +0200

    main color change

--- app/src/main/java/com/davide/seddio/easygallery/ui/theme/Color.kt
+++ app/src/main/java/com/davide/seddio/easygallery/ui/theme/Color.kt
@@ -10,5 +10,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val TopBarBlue = Color(24, 6, 158)
+val TopBarBlue = Color(0xFF017DDF)
 val BottomGrey = Color(5, 1, 31)
