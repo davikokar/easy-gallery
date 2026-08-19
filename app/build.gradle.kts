007d57e7973be58f42cc556bfb89cff8f7fc022f
davide <davide.seddio@gmail.com>
2026-08-19 21:33:00 +0200

    version increase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.davide.seddio.easygallery"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
