40214dc50c14a33a503fb99f42b493635b34c8e6
Davide Seddio <seddiod@emea.roche.com>
2026-09-23 18:47:59 +0200

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.davide.seddio.easygallery"
         minSdk = 28
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
